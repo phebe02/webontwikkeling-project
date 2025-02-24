920a7640b281c6020506e248c577ac5cbd517c04
phebe02 <phiebs.d@gmail.com>
2025-02-24 21:52:13 +0100

    filtered data functie werkt ook

--- porsche-project/index.ts
+++ porsche-project/index.ts
@@ -3,30 +3,39 @@ import { PorscheData } from './interfaces/interface';
 import readline from "readline-sync";
 import  porscheData  from './json/porsche.json';
 
+console.log("Welcome to the JSON data viewer!");
 //het hoofdmenu functie maken 
 function showMenu(): void {
-    console.log("Welcome to the JSON data viewer!");
-    console.log("");
+    console.log(""); 
+    console.log("Select an option of the menu:");
     console.log("1. View all data");
     console.log("2. Filter by ID");
     console.log("3. Exit");
   }
 
-  //hoofdmenu laten zien
-  showMenu();
-
 //keuze van de gebruiker vragen en de case aanroepen
-  const choice: number = readline.questionInt("Please enter your choice: ")
-switch (choice) {
-    case 1:
+
+const choice: number = 0;
+  do {
+
+    showMenu();
+    const choice: number = readline.questionInt("Please enter your choice: ");
+    switch (choice) {
+      case 1:
         viewData();
         break;
-        case 2:
+      case 2:
         filteredData();
         break;
-    default:
+      case 3:
+        console.log("Goodbye!");
+        process.exit(0);
         break;
-}
+      default:
+        console.log("Invalid choice. Please try again.");
+    }
+  } while (choice !== 3);
+
 
   //functie case 1 alle data 
   function viewData(): void {
@@ -39,6 +48,25 @@ switch (choice) {
   //functie case 2 gefilterde data
   function filteredData(){
     const filterId: number= readline.questionInt("Please enter the ID you want to filter by: ")
+    const data: PorscheData = porscheData;
 
+    const foundPorsche = data.porsches.find(porsche => porsche.id === filterId);
+  
+    if (foundPorsche) {
+      console.log(`- ${foundPorsche.name} (ID: ${foundPorsche.id})`);
+      console.log(`  - Description: ${foundPorsche.description}`);
+      console.log(`  - Horsepower: ${foundPorsche.horsepower}`);
+      console.log(`  - Is Electric: ${foundPorsche.isElectric}`);
+      console.log(`  - Release Date: ${foundPorsche.releaseDate}`);
+      console.log(`  - Image: ${foundPorsche.imageUrl}`);
+      console.log(`  - Category: ${foundPorsche.category}`);
+      console.log(`  - Features: ${foundPorsche.features.join(", ")}`);
+      console.log(`  - Manufacturer: ${foundPorsche.manufacturer.name}`);
+      console.log(`    - Country: ${foundPorsche.manufacturer.country}`);
+      console.log(`    - Founded: ${foundPorsche.manufacturer.founded}`);
+      console.log(`    - Logo: ${foundPorsche.manufacturer.logoUrl}`);
+    } else {
+      console.log(`Geen data gevonden voor ID ${filterId}.`);
+    }
 
   }
